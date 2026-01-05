74de943ebfaa7d9024373c1627d7d3987677e1b4
fenugrec <fenugrec@users.sourceforge.net>
2026-01-05 10:47:04 -0500

    decode some switch fields

--- presonus.lua
+++ presonus.lua
@@ -1,5 +1,6 @@
 -- Presonus Studio dissector for wireshark
 -- (c) fenugrec 2025
+-- GPLv3
 --
 -- place or symlink in $HOME/.local/lib/wireshark/plugins
 -- to reload : Analyze->reload LUA plugins
@@ -11,14 +12,16 @@ studiousb_protocol = Proto("studiousb", "Presonus StudioUSB protocol")
 
 -- all fields must be 'registered' even if they may be missing e.g. payload
 seq_id = ProtoField.uint32("studiousb.seq_id" , "seq_id" )
-sel = ProtoField.uint32("studiousb.sel" , "sel" , base.HEX)
+-- not sure how to combine this inline LUT with the later sel_table[]
+sel = ProtoField.uint32("studiousb.sel" , "sel" , base.DEC, {
+	[0] = "device",
+	[0x64] = "mixer",
+	[0x65] = "output",
+})
 u32 = ProtoField.uint32("studiousb.u32", "generic u32", base.HEX)
+bflag = ProtoField.bool("studiousb.bflag", "bool", base.NONE, {[1] = "0", [2] = "1"})
 
-studiousb_protocol.fields = { seq_id, sel, u32}
-
--- can we do a generic u32 field ?
--- studiousb_protocol.fields.u32 = ProtoField.uint32("studiousb.u32", "generic u32", base.HEX)
-
+studiousb_protocol.fields = { seq_id, sel, u32, bflag}
 
 -- ******************* logging
 -- sauce: https://wiki.wireshark.org/Lua/Examples/PostDissector
@@ -56,6 +59,15 @@ SC1810C_GET_STATE_REQ = 162
 SC1810C_GET_STATE_F1 = SC1810C_SET_STATE_F1
 SC1810C_GET_STATE_F2 = SC1810C_SET_STATE_F2
 
+-- these are indices inside the uint32[63] array composing the 'state' packet
+statefield_table = {
+	[58] = "48V SW",
+	[59] = "Line SW",
+	[60] = "Mute SW",
+	[61] = "Mono SW",
+	[62] = "A/B SW",
+}
+
 -- ****************************************
 -- 'core' of the dissector
 -- loosely based on "fpm.lua" example from wireshark wiki and netdaq dissector
@@ -68,25 +80,30 @@ SC1810C_GET_STATE_F2 = SC1810C_SET_STATE_F2
 -- Not sure how to get the first 7 bytes to be parsed by the basic USB decoder...
 function studiousb_protocol.dissector(buf, pinfo, tree)
 	length = buf:len()
-	if length < (7*4) then return 0 end
+	if length ~= 252 then return 0 end
 	local log = initLog(tree,studiousb_protocol)
 	log(string.format('len %u', length))
 
 	pinfo.cols.protocol = studiousb_protocol.name
 	local subtree = tree:add(studiousb_protocol, buf(), "StudioUSB Protocol Data")
 
-	local selector = buf(0,4):uint() -- field 'a' in kernel code
-	local field_b = buf(4,4):uint()
-	local field_f1 = buf(8,4):uint()
-	local field_f2 = buf(12,4):uint()
+	local selector = buf(0,4):le_uint() -- field 'a' in kernel code
+	local field_b = buf(4,4):le_uint()
+	local field_f1 = buf(8,4):le_uint()
+	local field_f2 = buf(12,4):le_uint()
 
 	subtree:add(sel, buf(0,4))
 	selstring = sel_table[selector].name
 	pinfo.cols.info:append(string.format(';sel %X(%s)', selector, selstring))
 	subtree:add(u32, buf(4,4)):set_text(string.format('b field: %X', field_b))
 	-- TODO : validate F1 or other
-	subtree:add(u32, buf(8,4)):set_text(string.format('F1 marker: %X', field_f1))
+	subtree:add_le(u32, buf(8,4)):set_text(string.format('F1 marker: %X', field_f1))
+	subtree:add_le(u32, buf(12,4)):set_text(string.format('F2 marker: %X', field_f2))
 
+	-- other fields : label if known
+	for field_idx,field_text in pairs(statefield_table) do
+		subtree:add(bflag, buf(field_idx*4,4)):append_text(string.format(' (%s)', field_text))
+	end
 --	-- info colum : always start with seq number
 --	pinfo.cols.info = string.format('seq=%u', seq_id_uint)
 --	-- generic command header.
@@ -94,9 +111,13 @@ function studiousb_protocol.dissector(buf, pinfo, tree)
 
 --	subtree:add(cmd, buf(8,4)):append_text(string.format(' (%s)', cmdstring ))
 
-	return 0 --length
+	return length
 end
 
+set_plugin_info({
+	version = "1.0",
+	author = "fenugrec",
+})
 --- menu View->Internal->Dissector tab
 --- also see epan/dissectors/packet-usb.c . Clear as mud
 --- Also doesn't work well
